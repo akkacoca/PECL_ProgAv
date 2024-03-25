8c5aaccc284cf52093af9b1c3e1fdc08dbf08cae
akkacoca <daniccoca@gmail.com>
2024-03-25 19:31:14 +0100

    prueba 1

---
+++ src/main/java/com/mycompany/pecl/Pecl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pecl;
+
+/**
+ *
+ * @author danic
+ */
+public class Pecl {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
