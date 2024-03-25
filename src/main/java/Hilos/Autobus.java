a32a69b5532e782ed56170f4b4a3960bb8c31c10
akkacoca <daniccoca@gmail.com>
2024-03-25 20:00:35 +0100

    1

---
+++ src/main/java/Hilos/Autobus.java
@@ -0,0 +1,17 @@
+package Hilos;
+
+public class Autobus extends Thread{
+    private String identificador;
+    /*private Aeropuerto aeropuerto;*/
+
+
+    /*public Autobus(String identificador, Aeropuerto aeropuerto) {
+        this.identificador = identificador;
+        this.aeropuerto = aeropuerto;
+    }*/
+
+    @Override
+    public void run() {
+        while (true) {
+        }
+}
