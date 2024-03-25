8c8ee82a5e9fae7950689e7f9a449a0f9fd65212
akkacoca <daniccoca@gmail.com>
2024-03-25 19:35:35 +0100

    prueba 2

--- src/main/java/com/mycompany/pecl/Pecl.java
+++ src/main/java/com/mycompany/pecl/Pecl.java
@@ -11,6 +11,6 @@ package com.mycompany.pecl;
 public class Pecl {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("MeCagoEnLosPutosMuertosQueParioElGitHubDeLosCojones");
     }
 }
